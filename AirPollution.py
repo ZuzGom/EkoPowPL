08f8bed278fcf604b9668663766d63e9d67b4408
= <jakubfraczek.m8@gmail.com>
2020-12-17 20:58:01 +0100

    Idea with water albedo

--- AirPollution.py
+++ AirPollution.py
@@ -3,32 +3,53 @@
 from PIL import Image #Pillow library
 from math import sqrt
 img = Image.open("test.jpg")
-img = img.convert ('RGB')
+img = img.convert('RGB')
 
-def imageBrightness(): # converts image into Grayscale
-    temp = img.copy()  # creating copy, so it won't overwrite original picture
+temp = img.copy()  # creating copy, so it won't overwrite original picture
+pixels = temp.load()
+
+def findWater():
+    #water albedo 5-10%
+    #NDWI=(G-IR)/(G+IR)
+
+    NDWIsuma=0
+    NDWI=0
+    BrightnessSum = 0  # used for calculating average water brightness
+    pixelCounter = 0  # counting water pixels
     for X in range(0, 1296): #image width
         for Y in range(0, 972): #image height
             pixelRGB = img.getpixel((X,Y)) # Get pixel RGB values
             R,G,B = pixelRGB #divide RBG into sigle variables
-            Brightness = sum([R,G,B])/3 ##0 is dark (black) and 255 is bright (white)
-            #print("B",Brightness)
-            pixelRGB = int(Brightness),int(Brightness),int(Brightness) #set all: R,G,B to the same value to get Grayscale
-            temp.putpixel((X,Y),pixelRGB) #Set Pixels
-    temp.save("Processed.jpg") #saves picture
 
-def imageLuminanceA(): # converts image into Grayscale
-    temp = img.copy()  # creating copy, so it won't overwrite original picture
-    for X in range(0, 1296): #image width
-        for Y in range(0, 972): #image height
-            pixelRGB = img.getpixel((X,Y)) # Get pixel RGB values
-            R,G,B = pixelRGB #divide RBG into sigle variables
-            LuminanceA = (0.2126*R) + (0.7152*G) + (0.0722*B)
-            #print("L",LuminanceA)
-            pixelRGB = int(LuminanceA),int(LuminanceA),int(LuminanceA) #set all: R,G,B to the same value to get Grayscale
-            temp.putpixel((X,Y),pixelRGB) #Set Pixels
-    temp.save("ProcessedL.jpg") #saves picture
+            if (G+R)!=0: #avoids division by zero
+                NDWI=(G-R)/(G+R)
+            else:
+                NDWI=0
 
 
-imageLuminanceA()
-imageBrightness()
+            #PL: Tutaj algorytm fajnie działa dla zaznaczania wody, ale zaznacza również fragmenty okna
+            # dlatego zauważyłem że okno jest quite ciemne, wiec jak Brightness jest małe to można pominąć
+            # zaznaczanie fragmentu okna, dzięki temu mi nie wydupi tego co chcę potem zrobić
+            Brightness = sum([R, G, B]) / 3  ##0 is dark (black) and 255 is bright (white)
+            if Brightness < 35:
+                pixels[X,Y] = (0, 0, 0)
+            else:
+                #NDWIsuma = NDWI + NDWIsuma
+                if NDWI > 0.05 and NDWI < 0.5: #NDWI > 0.05 works really great
+                    pixels[X,Y] = (128, 234, 255)  #blue XD, visualization, so we can check how it works
+                    BrightnessSum = BrightnessSum+Brightness
+                    pixelCounter=pixelCounter+1
+    return BrightnessSum,pixelCounter
+    #NDWIsuma=NDWIsuma/(1296*972) #image size
+    #print(NDWIsuma)
+
+
+
+
+BrightnessSum,pixelCounter=findWater()
+
+if pixelCounter!=0:
+    print(BrightnessSum/pixelCounter)
+
+
+temp.save("ProcessedL.jpg")  # saves picture

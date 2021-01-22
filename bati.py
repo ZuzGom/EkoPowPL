3903058b52fcdde9a1df34b621eecd35e54962d3
ddeexxiikk <mc.jesse.sm@gmail.com>
2021-01-22 09:42:30 +0100

    Dane sie pobieraja jesli skrypt laczy sie ze strona

--- bati.py
+++ bati.py
@@ -3,7 +3,18 @@ from bs4 import BeautifulSoup
 
 page = requests.get('http://www.celestrak.com/NORAD/elements/stations.txt')
 if(page.status_code==200):
-    calosc = str(page.text)
+    calosc = page.text
     soup = BeautifulSoup(page.content, 'html.parser')
-    print(calosc)
+    #calosc.split('\n').[:3]
+
+    MyFile = open("kordynaty.txt", "w")
+    MyFile.write(calosc)
+    MyFile.close()
+
+    MyFile = open("kordynaty.txt", "r")
+    for i in range(6):
+        print(MyFile.readline())
+    MyFile.close()
+else:
+    print("Strona nie dziala")
 

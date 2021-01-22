929af50c914457dd881098e3f4da73d319770522
ddeexxiikk <mc.jesse.sm@gmail.com>
2021-01-22 13:20:48 +0100

    Trzeba zrobić pobieranie co jakiś czas

--- bati.py
+++ bati.py
@@ -1,7 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
 
-
 def track():
     page = requests.get('http://www.celestrak.com/NORAD/elements/stations.txt')
     if (page.status_code == 200):
@@ -13,5 +12,3 @@ def track():
         line2 = calosc[2]
 
         return name, line1, line2
-
-# przenios³am to do kordów

5da3dbbeacfb9cac35068c42b3bc8c809f2d50cb
ZuzGom <74205927+ZuzGom@users.noreply.github.com>
2021-01-23 14:56:15 +0100

    Update panorama.py

--- panorama.py
+++ panorama.py
@@ -16,13 +16,14 @@ names = []  # nazwy plików
 # https://pillow.readthedocs.io/en/stable/reference/Image.html
 
 # zdjęcia niech będą w formacie:
-# gg:mm:ss_lat_long.jpg
+# gg.mm.ss_lat_long.jpg
 for infile in glob.glob("*.jpg"):  # czyta każde zdjęcie w lokalizacji o rozszerzeniu.jpg
 
     file, ext = os.path.splitext(infile)
     names.append(file)  # nazwy plików bez rozszerzenia, przydadzą się poźniej
 
     img = Image.open(infile)
+    w, h = img.size
 
     im = img.copy()  # po co kopia? nie wiem
     im = im.convert('RGBA')
@@ -35,79 +36,97 @@ for infile in glob.glob("*.jpg"):  # czyta każde zdjęcie w lokalizacji o rozsz
             brightness = (r + g + b)/3
             if brightness < 50:  # jesli jest czarno, przeroczystość na maksa, niestety cienie chmur odpadają
                 px[X, Y] = (200, 200, 200, 0)
-    cord.append(file.split('_'))  # lista z kordami
     im.save("new_"+file + ".png")  # nowe zdjęcie bez ramki
+    file = file.split('_')
+    file[0] = file[0].split('.')
+    cord.append(file)  # lista z kordami
     print(im)  # czytam czy się wszystko udało
     c += 1  # liczy ile zdjęć będzie łączonych (jaki musi być rozmiar wyniku)
 
+print(cord)
+
+for i in range(len(cord)):
+    cord[i][0] = (int(cord[i][0][0]) * 3600 +
+                  int(cord[i][0][1]) * 60 +
+                  int(cord[i][0][2]))
+
+print(cord)
+
 # skomentowałam to bo wywalał błąd
-'''
+
 print(cord)
 for i in range(1, len(cord)):
-    try:
-        move.append([float(cord[-i][1])-float(cord[-i-1][1]),
-                 float(cord[-i][2])-float(cord[-i-1][2])])
+    move.append([cord[-i][0] - cord[-i - 1][0],
+                 float(cord[-i][1]) - float(cord[-i - 1][1]),
+                 float(cord[-i][2]) - float(cord[-i - 1][2])])
+    print(float(cord[-i][1]))
+    print(float(cord[-i - 1][1]))
+    print('bum')
     
 print(move)
 print(c)
-'''
 
-p = 410  # przesunięcie x
-q = 90  # przesunięcie y
+czy = input('y/n: ')
 
-size = (1920 + c*p, 1080+c*q)  # c * p, bo tyle przesunięć ile zdjęć z założeniem że przesunięcie jest takie samo
-print(size)
+if czy == 'y':
+    p = 410  # przesunięcie x
+    q = 90  # przesunięcie y
+
+    size = (
+    1920 + c * p, 1080 + c * q)  # c * p, bo tyle przesunięć ile zdjęć z założeniem że przesunięcie jest takie samo
+    print(size)
+
+    wynik = Image.new('RGBA', size)  # zdjęcie końcowe
+    wpx = wynik.load()  # piksele tegoż
+
+    try:
+        new = Image.open("new_" + names[0] + ".png")  # otwieram pierwszy png
+    except IndexError:
+        print('brak zdjęć!')
+    else:
+        # do wynikowego zdjęcia wpisuję pierwsze z nich
+        for X in range(0, w):  # width
+            for Y in range(0, h):  # height
+                wpx[X, Y] = new.getpixel((X, Y))
 
 
-wynik = Image.new('RGBA', size)  # zdjęcie końcowe
-wpx = wynik.load()  # piksele tegoż
+        def przesuniecie(a, b, new2):
+            # funckja porównuje zdjęcie po przesunięciu z wynikiem
+            for X in range(0, w):  # width
+                for Y in range(0, h):  # height
+                    try:
+                        p = wynik.getpixel((X + a, Y + b))  # wygląda to tak:
+                        # porównywanie nowego zdjęcia zaczyna się w miejscu wyniku po przsunięciu a,b
+                        p2 = new2.getpixel((X, Y))
+                        r1, g1, b1, a1 = p
+                        r2, g2, b2, a2 = p2
+                        if a2 > 0:
+                            if a1 > 0:
+                                if p == p2:
+                                    wpx[X + a, Y + b] = p
+                                else:
+                                    wpx[X + a, Y + b] = (int((r1 + r2) / 2),
+                                                         int((g1 + g2) / 2),
+                                                         int((b1 + b2) / 2), 300)
+                            else:
+                                wpx[X + a, Y + b] = p2
 
-
-new = Image.open("new_"+names[0]+".png")  # otwieram pierwszy png
-
-# do wynikowego zdjęcia wpisuję pierwsze z nich
-for X in range(0, w):  # width
-    for Y in range(0, h):  # height
-        wpx[X, Y] = new.getpixel((X, Y))
-
-
-def przesuniecie(a, b, new2):
-    # funckja porównuje zdjęcie po przesunięciu z wynikiem
-    for X in range(0, w):  # width
-        for Y in range(0, h):  # height
-            try:
-                p = wynik.getpixel((X + a, Y + b))  # wygląda to tak:
-                # porównywanie nowego zdjęcia zaczyna się w miejscu wyniku po przsunięciu a,b
-                p2 = new2.getpixel((X, Y))
-                r1, g1, b1, a1 = p
-                r2, g2, b2, a2 = p2
-                if a2 > 0:
-                    if a1 > 0:
-                        if p == p2:
-                            wpx[X + a, Y + b] = p
-                        else:
-                            wpx[X + a, Y + b] = (int((r1 + r2) / 2),
-                                                 int((g1 + g2) / 2),
-                                                 int((b1 + b2) / 2), 255)
-                    else:
+                    except IndexError:  # błąd zawsze może się zdażyć
+                        p2 = new2.getpixel((X, Y))
                         wpx[X + a, Y + b] = p2
 
-            except IndexError:  # błąd zawsze może się zdażyć
-                p2 = new2.getpixel((X, Y))
-                wpx[X + a, Y + b] = p2
 
-
-'''
-przesuniecie(350, 45, new2)
-przesuniecie(2*350, 2*45, new3)
-'''
-# przesunięcia takie same więc daje for
-for i in range(1, c):
-    new1 = Image.open("new_"+names[i]+".png")
-    przesuniecie(i*p, i*q, new1)
+        '''
+        przesuniecie(350, 45, new2)
+        przesuniecie(2*350, 2*45, new3)
+        '''
+        # przesunięcia takie same więc daje for
+        for i in range(1, c + 1):
+            new1 = Image.open("new_" + names[i] + ".png")
+            przesuniecie(i * p, i * q, new1)
+        wynik.save("wynik_new.jpg")
+        print(wynik)
 
 # można w tym miejscu usunąć wszystkie png
 for infile in glob.glob("*.png"):
     os.remove(infile)
-print(new)
-wynik.save("wynik_new.png")

90e2f89dfdedcb44153aad74e3ad1bbb31c8d775
ZuzGom <zuzgom@gmail.com>
2021-01-25 20:23:09 +0100

    zabieram wam wszelką zabawę

--- panorama.py
+++ panorama.py
@@ -1,7 +1,7 @@
 from PIL import Image
 import glob
 import os
-
+import matplotlib.pyplot as plt
 
 cord = []  # to służy do zapisu koordynatów z nazwy pliku
 move = []  # a to do ich przesunięcia
@@ -24,19 +24,23 @@ for infile in glob.glob("*.jpg"):  # czyta każde zdjęcie w lokalizacji o rozsz
 
     img = Image.open(infile)
     w, h = img.size
+    im = img.copy()  # po co kopia? nie wiem
 
     im = img.copy()  # po co kopia? nie wiem
     im = im.convert('RGBA')
+    '''
+    # raz powstałe nie muszą się nadpisywać
     px = im.load()  # piksele im
-
     for X in range(0, w):  # width
         for Y in range(0, h):  # height
             pixel_rgb = im.getpixel((X, Y))  # Get pixel's RGB values
             r, g, b, a = pixel_rgb
-            brightness = (r + g + b)/3
+            brightness = (r + g + b) / 3
             if brightness < 50:  # jesli jest czarno, przeroczystość na maksa, niestety cienie chmur odpadają
                 px[X, Y] = (200, 200, 200, 0)
-    im.save("new_"+file + ".png")  # nowe zdjęcie bez ramki
+    im.save("new_" + file + ".png")  # nowe zdjęcie bez ramki
+    '''
+
     file = file.split('_')
     file[0] = file[0].split('.')
     cord.append(file)  # lista z kordami
@@ -44,31 +48,43 @@ for infile in glob.glob("*.jpg"):  # czyta każde zdjęcie w lokalizacji o rozsz
     c += 1  # liczy ile zdjęć będzie łączonych (jaki musi być rozmiar wyniku)
 
 print(cord)
-
 for i in range(len(cord)):
     cord[i][0] = (int(cord[i][0][0]) * 3600 +
                   int(cord[i][0][1]) * 60 +
                   int(cord[i][0][2]))
 
-print(cord)
-
 # skomentowałam to bo wywalał błąd
 
-print(cord)
+
 for i in range(1, len(cord)):
-    move.append([cord[-i][0] - cord[-i - 1][0],
+    czas = cord[-i][0] - cord[-i - 1][0]
+    if czas > 80000:
+        czas = 86400 - cord[-i][0] + cord[-i - 1][0]
+    move.append([czas,
                  float(cord[-i][1]) - float(cord[-i - 1][1]),
                  float(cord[-i][2]) - float(cord[-i - 1][2])])
-    print(float(cord[-i][1]))
-    print(float(cord[-i - 1][1]))
-    print('bum')
-    
+move = move[::-1]
 print(move)
 print(c)
+x = []
+y = []
+
+if float(cord[0][1]) < 0:
+    names = names[::-1]
+for d in cord:
+    x.append(float(d[1]))
+    y.append(float(d[2]))
+print(x, y)
+plt.plot(x, y, 'ro')
+# plt.axis([0, 10, 0, 10])
+# plt.show()
 
 p = 410  # przesunięcie x
 q = 90  # przesunięcie y
-size = (1920 + c * p, 1080 + c * q)  # c * p, bo tyle przesunięć ile zdjęć z założeniem że przesunięcie jest takie samo
+# p = 350
+# q = 45
+size = (1920 + c * p + 500,
+        1080 + c * q + 200)  # c * p, bo tyle przesunięć ile zdjęć z założeniem że przesunięcie jest takie samo
 print(size)
 
 wynik = Image.new('RGBA', size)  # zdjęcie końcowe
@@ -83,8 +99,11 @@ else:
         for Y in range(0, h):  # height
             wpx[X, Y] = new.getpixel((X, Y))
 
+    wynik.save("wynik0_new.png")
+
 
     def przesuniecie(a, b, new2):
+        print(a, b)
         # funckja porównuje zdjęcie po przesunięciu z wynikiem
         for X in range(0, w):  # width
             for Y in range(0, h):  # height
@@ -108,20 +127,46 @@ else:
                 except IndexError:  # błąd zawsze może się zdażyć
                     p2 = new2.getpixel((X, Y))
                     wpx[X + a, Y + b] = p2
+        print("save" + str(i))
+        wynik.save("wynik" + str(i) + "_new.png")
 
 
     '''
+    #peóby zabawy z czasem
+    past1 = 0
+    past2 = 1
+
+    for i in range(1, c+1):
+        new1 = Image.open("new_" + names[i] + ".png")
+        przesuniecie(past1 + int(move[i-1][0] * 31.7), past2 + int(move[i-1][0] * 7), new1)
+        past1 += int(move[i-1][0] * 31.5)
+        past2 += int(move[i-1][0] * 6.5)
+
     przesuniecie(350, 45, new2)
     przesuniecie(2*350, 2*45, new3)
     '''
     # przesunięcia takie same więc daje for
     for i in range(1, c + 1):
         new1 = Image.open("new_" + names[i] + ".png")
-        przesuniecie(i * p, i * q, new1)
+        if i == 4:
+            print('lol')
+            # przesuniecie(i * p, i * q, new1)
+            przesuniecie(i * p + 66, i * q + 18, new1)
+        else:
+            if i == 5:
+                przesuniecie(i * p + 58, i * q + 16, new1)
+            else:
+                if i == 6:
+                    przesuniecie(i * p + 66 - 37 + 104, i * q + 16 - 2 + 21, new1)
+                else:
+                    print('yo')
+                    przesuniecie(i * p, i * q, new1)
+
+'''
+# można w tym miejscu usunąć wszystkie png
+    for x in names:
+        os.remove("new_" + x + ".png")
+    print(new)
     wynik.save("wynik_new.png")
     print(wynik)
-
-
-# można w tym miejscu usunąć wszystkie png
-for infile in glob.glob("*.png"):
-    os.remove(infile)
+'''

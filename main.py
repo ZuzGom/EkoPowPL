635a5cb702b47330bbd1fd9f52862315ac7d868e
ZuzGom <zuzgom@gmail.com>
2021-01-29 17:10:56 +0100

    main

--- main.py
+++ main.py
@@ -2,28 +2,41 @@ from time import sleep
 from picamera import PiCamera
 import ndvi
 from PIL import Image
+import glob
+from kordy import isstrack
+import datetime
+
+now = datetime.datetime.now()
+date = now.strftime("%D_%H.%M.%S_")
+lon, lat = isstrack()
+date += str(lon) + '_' + str(lat)
+
 
 camera = PiCamera()
 camera.resolution = (640, 480)
 camera.start_preview()
 # Camera warm-up time
 sleep(2)
-camera.capture("image.jpg")
-#image = Image.open("2image.jpg")
-#ndvi.index_convert(image)
+camera.capture(date + ".jpg")
 
-#def capture_film():
+
+for infile in glob.glob("*.jpg"):
+    image = Image.open(date + ".jpg")
+    ndvi.index_convert(image)
 
 
 def film_hd():
     camera.resolution = (1920, 1080)
     camera.framerate = 30
-    camera.start_recording('video1.h264')
+    camera.start_recording('video' + date +'.h264')
     camera.wait_recording(5)
+    camera.stop_recording()
+    camera.start_recording('video' + date + '.h264')
+    camera.wait_recording(60)
     camera.stop_recording()
 
 
-film_hd()
+# film_hd()
 
 '''
 # creating output folder

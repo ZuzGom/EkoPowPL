f980b53c6e989d3f71d344f0adbadc2712ece62f
ZuzGom <zuzgom@gmail.com>
2021-02-14 17:07:01 +0100

    daje commit bo usunelam omylkowo przed chwila caly postep ;c

--- main.py
+++ main.py
@@ -18,10 +18,11 @@ from rgb import if_black
 
 
 ### funkcja sprawdzajaca czy zdjecie jest czarne gotowa (linia 41)
+sys.stdout = open('EkoPowPL.log', 'w')
 
 sHat.welcomeMessage()
 
-sys.stdout = open('EkoPowPL.log', 'w')
+start = datetime.datetime.now()
 
 path = sys.path
 bytes_avail = psutil.disk_usage(path[0]).free
@@ -31,29 +32,25 @@ for x in path:
 print('Space left (MB): ')
 print(megabytes_avail)
 
-now = datetime.datetime.now()
-date = now.strftime("%m.%d_%H.%M.%S_")
-lon, lat, country = isstrack()
-date += str(int(lon)) + '_' + str(int(lat)) + '_' + country
-
 camera = PiCamera()
-camera.resolution = (2048,1536)
-camera.start_preview()
-# Camera warm-up time
-sleep(2)
-sHat.camera()
-name = 'image/'+date + '.jpg'
-camera.capture(name)
-if if_black(name):
-    print('Noc')
-    sHat.nightTime()
-sHat.hourglass_s1()
-dane = index_convert(name)
-sHat.hourglass_s2()
 
-sHat.hourglass_s3()
-lightIntensity(name, date)
-sHat.hourglass_s4()
+
+def high_def(s, img):
+    camera.resolution = (2048, 1536)
+    camera.start_preview()
+    # Camera warm-up time
+    sleep(s)
+    sHat.camera()
+    camera.capture(img)
+
+
+def low_def(s, img):
+    camera.resolution = (480, 320)
+    camera.start_preview()
+    # Camera warm-up time
+    sleep(s)
+    sHat.camera()
+    camera.capture(img)
 
 
 def film_hd(s):
@@ -62,6 +59,31 @@ def film_hd(s):
     camera.start_recording('video/' + date + '.h264')
     camera.wait_recording(s)
     camera.stop_recording()
+
+
+while True:
+    now = datetime.datetime.now()
+    date = now.strftime("%m.%d_%H.%M.%S_")
+    lon, lat, country = isstrack()
+    date += str(int(lon)) + '_' + str(int(lat)) + '_' + country
+
+    camera = PiCamera()
+
+    name = 'image/' + date + '.jpg'
+
+    if if_black(name):
+        print('Noc')
+        sHat.nightTime()
+    sHat.hourglass_s1()
+    dane = index_convert(name)
+    sHat.hourglass_s2()
+
+    sHat.hourglass_s3()
+    lightIntensity(name, date)
+    sHat.hourglass_s4()
+
+
+
 
 # film_hd(5)
 

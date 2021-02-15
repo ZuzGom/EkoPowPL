210511a989ca9b2015a0fe4f9b6d5792018bdbce
= <jakubfraczek.m8@gmail.com>
2021-02-15 18:56:12 +0100

    Ostatnie poprawki, bo mi się nie chce uczyć

--- LightIntensity.py
+++ LightIntensity.py
@@ -1,25 +1,31 @@
-# Amount of light reflected from Earth (intensity in lux), depending on air pollution
-# https://www.epa.gov/pm-pollution/particulate-matter-pm-basics
-#
-# Luminance is a photometric measure of the luminous intensity per unit area of light travelling in a given direction.
-# It describes the amount of light that passes through, is emitted from, or is reflected from a particular area,
-# and falls within a given solid angle.
-# reference: https://en.wikipedia.org/wiki/Luminance
-#
-# Relative luminance follows the photometric definition of luminance, but with the
-# values normalized to 1 or 100 for a reference white. Like the photometric definition,
-# it is related to the luminous flux density in a particular direction, which is radiant flux density
-# weighted by the luminosity function y(λ) of the CIE Standard Observer.
-# Y = 0.2126*R + 0.7152G + 0.0722B.
-# reference: https://en.wikipedia.org/wiki/Relative_luminance
+"""
+Amount of light reflected from Earth (intensity in lux), depending on air pollution
+https://www.epa.gov/pm-pollution/particulate-matter-pm-basics
 
-from PIL import Image  # Pillow library
+Luminance is a photometric measure of the luminous intensity per unit area of light travelling in a given direction.
+It describes the amount of light that passes through, is emitted from, or is reflected from a particular area,
+and falls within a given solid angle.
+reference: https://en.wikipedia.org/wiki/Luminance
+
+Relative luminance follows the photometric definition of luminance, but with the
+values normalized to 1 or 100 for a reference white. Like the photometric definition,
+it is related to the luminous flux density in a particular direction, which is radiant flux density
+weighted by the luminosity function y(λ) of the CIE Standard Observer.
+Y = 0.2126*R + 0.7152G + 0.0722B.
+reference: https://en.wikipedia.org/wiki/Relative_luminance
+"""
+
+"""
+Calculates Relative luminance for clouds and snow, water, land.
+Outputs data and saves it to '.csv' file. This data will be used for analysis on Earth.
+"""
+
+from PIL import Image
 import datetime
 
 def lightIntensity(name ,date):
 
     img = Image.open(name)
-
     img = img.convert('RGB')
 
     width = img.width
@@ -29,6 +35,7 @@ def lightIntensity(name ,date):
     imgWater = img.copy().convert('HSV')
     pixelsWater = imgWater.load()
 
+    # Relative luminance of given pixel
     RelativeLuminance = 0
 
     # AverageRelativeLuminance is responsible for average Relative Luminance of all picture, including window
@@ -55,9 +62,11 @@ def lightIntensity(name ,date):
     LandPixels = 0
     AverageLandRelativeLuminance = 0
 
-    # Loop #1, responsible for:
-    # - calculating average Luminance of all picture
-    # - separating water form picture, using NDWI index
+    """
+    Loop #1, responsible for:
+    - gathering information, which will be used for calculating average Luminance of all picture, including window
+    - separating water form picture, using NDWI index
+    """
 
     for X in range(0, width):  # image width
         for Y in range(0, height):  # image height
@@ -71,7 +80,7 @@ def lightIntensity(name ,date):
             RelativeLuminance = int(RelativeLuminance)
             AverageRelativeLuminance += RelativeLuminance
 
-            # Calculates NDWI and casts it to 0-360 (so far so good)
+            # Calculates NDWI and casts it to 0-360
             if(R+G) > 0:
                 NDWI = (R - G) / (R + G)
 
@@ -79,6 +88,7 @@ def lightIntensity(name ,date):
                 NDWI *= 180
                 NDWI += 180
 
+                # marks water areas, makes everything else black
                 if NDWI < 160:
                     # ~via Zuzia
                     pixelsWater[X, Y] = (int(NDWI), 200, 200)  # HSV
@@ -92,10 +102,12 @@ def lightIntensity(name ,date):
     # calculating average Luminance of whole picture, AverageRelativeLuminance divided by total pixels
     AverageRelativeLuminance /= width*height
 
+    """
     # Loop #2, responsible for:
-    # - calculating average Luminance of water + clouds + snow + land
+    # - gathering information, which will be used for calculating average Luminance of water + clouds and snow + land
     # - separating water from the rest of the picture
     # - separating window from the rest of the picture
+    """
 
     for X in range(0, width):  # image width
         for Y in range(0, height):  # image height
@@ -104,7 +116,7 @@ def lightIntensity(name ,date):
             pixelRGB = img.getpixel((X, Y))  # Get pixel RGB values
             R, G, B = pixelRGB  # divide RBG into sigle variables
 
-            # Liczy RelativeLuminance
+            # Calculates RelativeLuminance
             RelativeLuminance = (0.2126 * R) + (0.7152 * G) + (0.0722 * B)
             RelativeLuminance = int(RelativeLuminance)
 
@@ -113,7 +125,8 @@ def lightIntensity(name ,date):
             if RelativeLuminance < (AverageRelativeLuminance / 3):
                 pixelsWater[X, Y] = (0, 0, 0)
             else:
-                # if pixels are not considered as window then average Luminance for the rest of the picture is calculated
+                # if pixels are not considered as window then information
+                # which will be used for calculating average Luminance for the rest of the picture is gathered
                 AverageRelativeLuminance2 += RelativeLuminance
                 AverageRelativeLuminance2pixels += 1
 
@@ -123,33 +136,35 @@ def lightIntensity(name ,date):
             pixelRGBWater = imgWater.getpixel((X, Y))  # Get pixel RGB values
             Rw, Gw, Bw = pixelRGBWater  # divide RBG into single variables
 
-            if Rw != 0 or Gw != 0 or Bw != 0:  # RGB values for water
+            # RGB values for water, because in imgWater, other pixels but water are set to (0,0,0)
+            if Rw != 0 or Gw != 0 or Bw != 0:
                 WaterRelativeLuminance += RelativeLuminance
                 WaterPixels += 1
 
-            if R>=200: # separetes clouds and land
+            # separetes clouds and land, then gathers information used for calculating relative luminance for both of them
+            if R>=200:
                 CloudsRelativeLuminance += RelativeLuminance
                 CloudsPixels += 1
             else:
                 LandRelativeLuminance += RelativeLuminance
                 LandPixels += 1
 
-
+    # Calculating average relative luminance for: whole picture (excluding window), clouds + snow, water, land
     if AverageRelativeLuminance2pixels > 0:
         AverageRelativeLuminance2 /= AverageRelativeLuminance2pixels
 
     if WaterPixels > 0:
-        AverageWaterRelativeLuminance = WaterRelativeLuminance/WaterPixels
+        AverageWaterRelativeLuminance /= WaterPixels
 
     if CloudsPixels > 0:
-        AverageCloudsRelativeLuminance = CloudsRelativeLuminance/CloudsPixels
+        AverageCloudsRelativeLuminance /= CloudsPixels
 
     if LandPixels > 0:
-        AverageLandRelativeLuminance = LandRelativeLuminance / LandPixels
+        AverageLandRelativeLuminance /= LandPixels
 
     imageDatetime = datetime.datetime.now().strftime("%d.%m.%Y-%H:%M:%S")
 
-    data = open('LightIntensity.csv', 'w')
+    data = open('LightIntensity.csv', 'a')
     data.writelines("Datetime: ;"
                     "Date: ;"
                     "AverageRelativeLuminanceWithoutWindowBorders:    ;"
